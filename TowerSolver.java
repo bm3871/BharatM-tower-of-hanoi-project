336423656729ac53c49d1cea8a687d8cd79449ac
bm3871 <bm3871@pleasantonusd.net>
2025-03-25 21:30:51 +0000

    solveRecursive method called, declared, moving n-1 disks and nth disks

--- TowerSolver.java
+++ TowerSolver.java
@@ -10,13 +10,18 @@ public class TowerSolver {
     public void solve(TowerModel model)
     {
         this.model = model;
-        // Call the missing solve method (not this one).
+        solveRecursive(model.height(), 0, 2, 1);
     }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
+    private void solveRecursive(int n, int source, int destination, int auxiliary) {
+        if(n==1) {
+            model.move(source, destination);
+            return;
+        }
+
+        solveRecursive(n - 1, source, auxiliary, destination);
+        model.move(source, destination);
+        solveRecursive(n - 1, auxiliary, destination, source);
+    }
 
 }
